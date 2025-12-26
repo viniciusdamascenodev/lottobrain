864648141dd72784503ec75fd0202a8969b697ae
Vinícius Pedro Damasceno Lima <vinicius.damasceno.dev@gmail.com>
2025-12-26 21:51:40 +0000

    Atualiza dados loterias

--- scripts/testar_download.py
+++ scripts/testar_download.py
@@ -3,7 +3,7 @@ import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from app.loterias.mega_millions import baixar
+from app.loterias.lotofacil import baixar
 
 if __name__ == "__main__":
     caminho = baixar()
